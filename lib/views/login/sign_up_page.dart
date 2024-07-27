fa0d9eb06ec1df05bb283b5301867d48d485ee74
Joojinsung <dev.jinsung1017@gmail.com>
2024-07-27 17:04:54 +0900

    sign-up ui validator 설정

--- lib/views/login/sign_up_page.dart
+++ lib/views/login/sign_up_page.dart
@@ -1,6 +1,7 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:wadiz_clone/theme.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
@@ -10,10 +11,198 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUpPage> {
+  TextEditingController emailTextController = TextEditingController();
+  TextEditingController passwordTextController = TextEditingController();
+  TextEditingController password2TextController = TextEditingController();
+  TextEditingController usernameTextController = TextEditingController();
+
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    emailTextController.dispose();
+    passwordTextController.dispose();
+    password2TextController.dispose();
+    usernameTextController.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "이메일 간편가입",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                    color: AppColors.wadizGray[900],
+                  ),
+                ),
+                Gap(20),
+                Text(
+                  "이메일 계정",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                Gap(12),
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        controller: emailTextController,
+                        decoration: InputDecoration(
+                          hintText: "아이디입력",
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "필수입력항목입니다";
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Gap(12),
+                    GestureDetector(
+                      child: Container(
+                        height: 54,
+                        width: 90,
+                        decoration: BoxDecoration(
+                          color: AppColors.primaryColor.withOpacity(.55),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "인증하기",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Gap(20),
+                Text("이름",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    )),
+                Gap(12),
+                TextFormField(
+                  controller: usernameTextController,
+                  decoration: InputDecoration(
+                    hintText: "이름 입력",
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "필수입력항목입니다";
+                    }
+                    return null;
+                  },
+                ),
+                Gap(20),
+                Text("비밀번호",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    )),
+                Gap(12),
+                TextFormField(
+                  controller: passwordTextController,
+                  decoration: InputDecoration(
+                    hintText: "비밀번호 입력",
+                    filled: true,
+                    fillColor: AppColors.newBg,
+                    border: InputBorder.none,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    suffixIcon: IconButton(
+                      icon: Icon(Icons.visibility),
+                      onPressed: () {},
+                    ),
+                  ),
+                  obscureText: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "필수입력항목입니다";
+                    }
+                    return null;
+                  },
+                ),
+                Gap(12),
+                TextFormField(
+                  controller: password2TextController,
+                  decoration: InputDecoration(
+                    hintText: "비밀번호 확인",
+                    filled: true,
+                    fillColor: AppColors.newBg,
+                    border: InputBorder.none,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    suffixIcon: IconButton(
+                      icon: Icon(Icons.visibility),
+                      onPressed: () {},
+                    ),
+                  ),
+                  obscureText: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "필수입력항목입니다";
+                    }
+                    if (value != passwordTextController.text.trim()) {
+                      return "비밀번호가 다릅니다";
+                    }
+                    return null;
+                  },
+                ),
+                Gap(20),
+                GestureDetector(
+                  onTap: () {
+                    if (formKey.currentState!.validate()) {
+                      formKey.currentState?.save();
+                    }
+                  },
+                  child: Container(
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: AppColors.primaryColor,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "약관 동의 후 가입 완료하기",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
