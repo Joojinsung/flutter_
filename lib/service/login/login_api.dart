7791e9f9fa6e6321d2996bf4c5b85e10d7aac17a
Joojinsung <dev.jinsung1017@gmail.com>
2024-07-27 17:18:20 +0900

    로그인 api, service

---
+++ lib/service/login/login_api.dart
@@ -0,0 +1,18 @@
+import 'package:retrofit/retrofit.dart';
+import 'package:dio/dio.dart';
+import 'package:wadiz_clone/shared/model/response_model.dart';
+import '../../model/home/home_model.dart';
+import '../../model/login/login_model.dart'; // HomeModel 임포트 추가
+
+part 'login_api.g.dart';
+
+@RestApi(baseUrl: "https://localhost:3000/api/v1")
+abstract class LoginApiClient {
+  factory LoginApiClient(Dio dio, {String baseUrl}) = _LoginApiClient;
+
+  @POST("/signup")
+  Future<ResponseModel> signUp(
+    @Body() LoginModel loginModel,
+
+  );
+}
---
+++ lib/service/login/login_api_service.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'login_api_service.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$loginApiClientHash() => r'7bc7db09a9e69f4b39c6fc3eaf783b563dba849f';
+
+/// See also [loginApiClient].
+@ProviderFor(loginApiClient)
+final loginApiClientProvider = AutoDisposeProvider<LoginApiClient>.internal(
+  loginApiClient,
+  name: r'loginApiClientProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$loginApiClientHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef LoginApiClientRef = AutoDisposeProviderRef<LoginApiClient>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
