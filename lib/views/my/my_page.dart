a87609b9e506f3644a36163058165a36b33928d4
Joojinsung <dev.jinsung1017@gmail.com>
2024-07-28 02:31:12 +0900

    리워드 추가하기 화면

--- lib/views/my/my_page.dart
+++ lib/views/my/my_page.dart
@@ -8,10 +8,6 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:wadiz_clone/view_model/login/login_view_model.dart';
 import 'package:wadiz_clone/view_model/my/my_view_model.dart';
 
-
-
-
-
 class MyPage extends StatefulWidget {
   const MyPage({super.key});
 
@@ -23,93 +19,30 @@ class _MyPageState extends State<MyPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "my Page",
-          ),
-          titleTextStyle: TextStyle(
-            fontWeight: FontWeight.w600,
-            fontSize: 20,
-            color: AppColors.wadizGray[900],
-          ),
+      appBar: AppBar(
+        title: Text(
+          "my Page",
         ),
-        body: SingleChildScrollView(
-          child: Consumer(builder: (context, ref, child) {
-            final myPageState = ref.watch(myViewModelProvider);
+        titleTextStyle: TextStyle(
+          fontWeight: FontWeight.w600,
+          fontSize: 20,
+          color: AppColors.wadizGray[900],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Consumer(builder: (context, ref, child) {
+          final myPageState = ref.watch(myViewModelProvider);
 
-            return Column(
-              children: [
-                Container(
-                  height: 430,
-                  padding: EdgeInsets.all(16),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Consumer(builder: (context, ref, child) {
-                        if (myPageState?.loginState ?? false) {
-                          return Row(
-                            children: [
-                              CircleAvatar(
-                                radius: 24,
-                                backgroundColor: AppColors.bg,
-                              ),
-                              Gap(8),
-                              Expanded(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      "${myPageState?.loginModel?.email}",
-                                      style: TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.w600,
-                                      ),
-                                    ),
-                                    Gap(4),
-                                    Text(
-                                      "${myPageState?.loginModel?.username} 님 안녕하세요",
-                                      style: TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.w600,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              IconButton(
-                                tooltip: "로그아웃",
-                                onPressed: () {
-                                  showDialog(
-                                    context: context,
-                                    builder: (context) =>
-                                        AlertDialog(
-                                          content: Text("로그아웃 하시겠습니까?"),
-                                          actions: [
-                                            TextButton(
-                                              onPressed: () {
-                                                Navigator.of(context).pop();
-                                              },
-                                              child: Text("취소"),
-                                            ),
-                                            TextButton(
-                                              onPressed: () {
-                                                ref
-                                                    .read(loginViewModelProvider
-                                                    .notifier)
-                                                    .signOut();
-                                                Navigator.of(context).pop();
-                                              },
-                                              child: Text("확인"),
-                                            ),
-                                          ],
-                                        ),
-                                  );
-                                },
-                                icon: Icon(Icons.logout),
-                              ),
-                            ],
-                          );
-                        }
+          return Column(
+            children: [
+              Container(
+                height: 430,
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Consumer(builder: (context, ref, child) {
+                      if (myPageState?.loginState ?? false) {
                         return Row(
                           children: [
                             CircleAvatar(
@@ -117,103 +50,170 @@ class _MyPageState extends State<MyPage> {
                               backgroundColor: AppColors.bg,
                             ),
                             Gap(8),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                InkWell(
-                                  onTap: () {
-                                    context.push("/login");
-                                  },
-                                  child: Row(
-                                    children: [
-                                      Text("로그인하기"),
-                                      Icon(Icons.keyboard_arrow_right),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "${myPageState?.loginModel?.email}",
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                  Gap(4),
+                                  Text(
+                                    "${myPageState?.loginModel?.username} 님 안녕하세요",
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            IconButton(
+                              tooltip: "로그아웃",
+                              onPressed: () {
+                                showDialog(
+                                  context: context,
+                                  builder: (context) => AlertDialog(
+                                    content: Text("로그아웃 하시겠습니까?"),
+                                    actions: [
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: Text("취소"),
+                                      ),
+                                      TextButton(
+                                        onPressed: () {
+                                          ref
+                                              .read(loginViewModelProvider
+                                                  .notifier)
+                                              .signOut();
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: Text("확인"),
+                                      ),
                                     ],
                                   ),
-                                ),
-                                Gap(4),
-                                Text(
-                                  "로그인 후 다양한 프로젝트에 참여해보세요.",
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.w400,
-                                  ),
-                                ),
-                              ],
+                                );
+                              },
+                              icon: Icon(Icons.logout),
                             ),
                           ],
                         );
-                      }),
-                      Column(
+                      }
+                      return Row(
                         children: [
                           CircleAvatar(
-                            radius: 28,
-                            backgroundColor: AppColors.wadizGray[200],
-                            child: SvgPicture.asset(
-                              "assets/icons/featured_seasonal_and_gifts.svg",
-                              width: 28,
-                              height: 28,
-                              colorFilter: ColorFilter.mode(
-                                Colors.white,
-                                BlendMode.srcIn,
+                            radius: 24,
+                            backgroundColor: AppColors.bg,
+                          ),
+                          Gap(8),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              InkWell(
+                                onTap: () {
+                                  context.push("/login");
+                                },
+                                child: Row(
+                                  children: [
+                                    Text("로그인하기"),
+                                    Icon(Icons.keyboard_arrow_right),
+                                  ],
+                                ),
                               ),
-                            ),
-                          ),
-                          Gap(20),
-                          Text(
-                            "새로운 도전을\n시작해보세요",
-                            style: TextStyle(
-                              fontWeight: FontWeight.w700,
-                              fontSize: 13,
-                            ),
-                          ),
-                          Gap(20),
-                          Text(
-                            "개인 후원부터 제품, 콘텐츠, 서비스 출시, 성장까지 함께할께요.",
-                            style: TextStyle(
-                              fontWeight: FontWeight.w400,
-                              fontSize: 12,
-                            ),
+                              Gap(4),
+                              Text(
+                                "로그인 후 다양한 프로젝트에 참여해보세요.",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ],
                           ),
                         ],
-                      ),
-                      InkWell(
-                        onTap: () {
-                          // todo : 로그인 처리 확인
-                          if (!(myPageState!.loginState ?? true)) {
-                            showDialog(
-                              context: context,
-                              builder: (context) =>
-                                  AlertDialog(
-                                    content: Text("로그인이 필요한 서비스입니다"),
-                                  ),);
-                            return;
-                          }
-                          //todo : 프로젝트 추가화면으로 이동
-                        },
-                        child: Container(
-                          height: 50,
-                          decoration: BoxDecoration(
-                            color: AppColors.primaryColor,
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Center(
-                            child: Text(
-                              "프로젝트 만들기",
-                              style: TextStyle(
-                                fontWeight: FontWeight.w600,
-                                fontSize: 16,
-                                color: Colors.white,
-                              ),
+                      );
+                    }),
+                    Column(
+                      children: [
+                        CircleAvatar(
+                          radius: 28,
+                          backgroundColor: AppColors.wadizGray[200],
+                          child: SvgPicture.asset(
+                            "assets/icons/featured_seasonal_and_gifts.svg",
+                            width: 28,
+                            height: 28,
+                            colorFilter: ColorFilter.mode(
+                              Colors.white,
+                              BlendMode.srcIn,
                             ),
                           ),
                         ),
-                      )
-                    ],
-                  ),
+                        Gap(20),
+                        Text(
+                          "새로운 도전을\n시작해보세요",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 13,
+                          ),
+                        ),
+                        Gap(20),
+                        Text(
+                          "개인 후원부터 제품, 콘텐츠, 서비스 출시, 성장까지 함께할께요.",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    ),
+                    InkWell(
+                      onTap: () {
+                        context.push(
+                          "/add/reward/0",
+                          extra: "0",
+                        );
+                        // todo : 로그인 처리 확인
+                        if (!(myPageState!.loginState ?? true)) {
+                          showDialog(
+                            context: context,
+                            builder: (context) => AlertDialog(
+                              content: Text("로그인이 필요한 서비스입니다"),
+                            ),
+                          );
+                          return;
+                        }
+                        //todo : 프로젝트 추가화면으로 이동
+                      },
+                      child: Container(
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: AppColors.primaryColor,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "프로젝트 만들기",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
                 ),
-              ],
-            );
-          }),
-        ),);
+              ),
+            ],
+          );
+        }),
+      ),
+    );
   }
 }
--- lib/views/project/add_reward_page.dart
+++ lib/views/project/add_reward_page.dart
@@ -1,11 +1,13 @@
-
-
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:gap/gap.dart';
+import 'package:go_router/go_router.dart';
+import 'package:wadiz_clone/theme.dart';
 
 class AddRewardPage extends StatefulWidget {
   final String projectId;
-
 
   const AddRewardPage({super.key, required this.projectId});
 
@@ -14,12 +16,161 @@ class AddRewardPage extends StatefulWidget {
 }
 
 class _AddRewardPageState extends State<AddRewardPage> {
+  TextEditingController priceTextEditingController = TextEditingController();
+  TextEditingController titleTextEditingController = TextEditingController();
+  TextEditingController descriptionTextEditingController =
+  TextEditingController();
+
+  @override
+  void dispose() {
+    priceTextEditingController.dispose();
+    titleTextEditingController.dispose();
+    descriptionTextEditingController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("리워드 추가"),
         centerTitle: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "리워드 추가",
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 16,
+                ),
+              ),
+              Gap(12),
+              DottedBorder(
+                color: AppColors.wadizGray[200]!,
+                dashPattern: [5, 5],
+                borderType: BorderType.RRect,
+                radius: Radius.circular(10),
+                child: Container(
+                  height: 216,
+                  width: double.infinity,
+                  color: Color(0xfffbfbfb),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.add_circle,
+                      ),
+                      Gap(
+                        12,
+                      ),
+                      Text(
+                        "리워드를 추가해주세요",
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Gap(24),
+              Text(
+                "금액",
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 16,
+                ),
+              ),
+              Gap(12),
+              TextFormField(
+                controller: priceTextEditingController,
+                inputFormatters: [
+                  FilteringTextInputFormatter.digitsOnly,
+                ],
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  hintText: "0",
+                ),
+              ),
+              Gap(24),
+              Text(
+                "리워드명",
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 16,
+                ),
+              ),
+              Gap(12),
+              TextFormField(
+                controller: titleTextEditingController,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  hintText: "예시) [얼리버드] 베이직 이불, 베개 1세트",
+                ),
+                maxLength: 60,
+              ),
+              Text(
+                "리워드 설명",
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 16,
+                ),
+              ),
+              Gap(12),
+              TextFormField(
+                controller: descriptionTextEditingController,
+                keyboardType: TextInputType.multiline,
+                decoration: InputDecoration(
+                  hintText: "리워드 구성과 혜택을 간결하게 설명해 주세요.",
+                ),
+                maxLength: 400,
+                maxLines: 10,
+              ),
+              Gap(24),
+              SizedBox(
+
+                height: 50,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: OutlinedButton(
+                        onPressed: () {
+                          context.go("/my");
+                        },
+                        child: Text("취소"),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Gap(12),
+              SizedBox(
+                height: 50,
+                child: GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: AppColors.primaryColor,
+                    ),
+                    child: Center(
+                      child: Text(
+                        "추가",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
